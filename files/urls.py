6a2381b00f96a9a59247fba5d0c3e0b64974e95c
Rolf Håvard Blindheim <rhblind@gmail.com>
2012-11-19 13:32:54 +0100

    added download view

--- files/urls.py
+++ files/urls.py
@@ -1,14 +1,11 @@
 from django.conf.urls import patterns, url
 
-from files.views import AttachmentDetailView
+from files.views import AttachmentDetailView, AttachmentDownloadView
 
 urlpatterns = patterns("files.views",
     url(r"^view/(?P<slug>[-\w]+)/$", view=AttachmentDetailView.as_view(), name="view-attachment"),
 #    url(r"^add/(?P<pk>\d+)/$", view="", name="add-attachment"),
 #    url(r"^delete/(?P<pk>\d+)/$", view="", name="delete-attachment"),
-#    url(r"^download/(?P<pk>\d+)/$", view="", name="download-attachment")
-    
-# From bartTC
-#    url(r'^add-for/(?P<app_label>[\w\-]+)/(?P<module_name>[\w\-]+)/(?P<pk>\d+)/$', 'attachments.views.add_attachment', name="add_attachment"),
-#    url(r'^delete/(?P<attachment_pk>\d+)/$', 'attachments.views.delete_attachment', name="delete_attachment"),
+    url(r"^download/(?P<slug>[-\w]+)/$", view=AttachmentDownloadView.as_view(), name="download-attachment"),
 )
+
--- files/views.py
+++ files/views.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from django.views.generic.detail import DetailView
+from django.views.generic.detail import DetailView, SingleObjectMixin,\
+    BaseDetailView
 from files.models import Attachment
+from django.http import HttpResponse
 
 
 class AttachmentDetailView(DetailView):
+    """
+    Returns the details of an attachment.
+    """
     
     model = Attachment
     context_object_name = "attachment"
     template_name = "attachments/detail.html"
+
+
+class AttachmentDownloadView(BaseDetailView, SingleObjectMixin):
+    """
+    Returns the attachment file as a HttpResponse.
+    """
+    
+    model = Attachment
+    
+    def render_to_response(self, context):
+        obj = context["object"]
+        response = HttpResponse(obj.attachment.file, mimetype=obj.mimetype)
+        response["Content-Disposition"] = "inline; filename=%s" % obj.attachment.file
+        return response

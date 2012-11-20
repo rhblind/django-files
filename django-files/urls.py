5ae1a41640e685a141fb459e609e7e91abc84bc2
Rolf Håvard Blindheim <rhblind@gmail.com>
2012-11-20 20:11:40 +0100

    made demosite base url

--- django-files/urls.py
+++ django-files/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^', include('demosite.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^demosite/', include('demosite.urls')),
     url(r"^attachments/", include("files.urls")),
 )

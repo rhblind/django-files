dc40992b8f659daa7a83c118cf6b6f16fbab72e2
Rolf Håvard Blindheim <rhblind@gmail.com>
2012-11-27 15:37:20 +0100

    some demosite stuff

--- demosite/urls.py
+++ demosite/urls.py
@@ -7,4 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns("demosite.views",
     url(r"^$", view=ShapeListView.as_view(), name="home"),
     url(r"^basic-usage/$", view=ShapeListView.as_view(template_name="basic-usage.html"), name="basic-usage"),
+    url(r"^rendering/$", view=ShapeListView.as_view(template_name="rendering.html"), name="rendering"),
 )

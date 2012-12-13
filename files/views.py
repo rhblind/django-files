c86f872347acc3a739fd07b638c1e0771e138a38
Rolf Håvard Blindheim <rhblind@gmail.com>
2012-12-13 11:25:58 +0100

    added NextMixin to handle next value from POST data

--- files/views.py
+++ files/views.py
@@ -8,7 +8,7 @@ from django.template.context import RequestContext
 from django.template.loader import select_template
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.contenttypes.models import ContentType
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, ImproperlyConfigured
 from django.views.generic.edit import DeleteView, CreateView, UpdateView
 from django.views.generic.detail import DetailView, SingleObjectMixin,\
     BaseDetailView
@@ -20,7 +20,24 @@ from files import get_form
 from files.models import Attachment
 
 
-class AttachmentCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+class NextMixin(object):
+    """
+    A mixin which returns the first valid value from
+    either "next" from POST data or success_url. If neither
+    is available, try to get the absolute url from model.
+    """
+    def get_success_url(self):
+        url = self.request.POST.get("next", None) or self.success_url
+        if not url:
+            try:
+                url = self.object.get_absolute_url()
+            except AttributeError:
+                raise ImproperlyConfigured("No URL to redirect to. Provide a next value, success_url"
+                                           " or a get_absolute_url method on the Model.")
+        return url
+
+
+class AttachmentCreateView(LoginRequiredMixin, PermissionRequiredMixin, NextMixin, CreateView):
     """
     View responsible for creating new attachments.
     """
@@ -37,7 +54,6 @@ class AttachmentCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateVi
             ctype_pk, object_pk = data.get("content_type"), data.get("object_id")
             if ctype_pk is None or object_pk is None:
                 raise AttributeError("Missing content_type or object_id field.")
-            
             model = ContentType.objects.get_for_id(ctype_pk)
             target = model.get_object_for_this_type(pk=object_pk)
         except AttributeError:
@@ -49,7 +65,6 @@ class AttachmentCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateVi
                     (ctype_pk, object_pk, e.__class__.__name__))
         except Exception, e:
             raise e
-        
         return form_class(target, **kwargs)
     
     def form_valid(self, form):
@@ -59,7 +74,7 @@ class AttachmentCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateVi
         return super(AttachmentCreateView, self).form_valid(form)
     
 
-class AttachmentEditView(LoginRequiredMixin, MultiplePermissionsRequiredMixin, UpdateView):
+class AttachmentEditView(LoginRequiredMixin, MultiplePermissionsRequiredMixin, NextMixin, UpdateView):
     """
     Updates an existing attachment with new data.
     """
@@ -87,7 +102,7 @@ class AttachmentEditView(LoginRequiredMixin, MultiplePermissionsRequiredMixin, U
                 "attachments/%s/edit_form.html" % ctype.model,
                 "attachments/edit_form.html"
             ]
-            names = [p for p in template_search_list] + names
+            names = template_search_list + names
         return names
 
 
@@ -100,7 +115,7 @@ class AttachmentDetailView(DetailView):
     template_name = "attachments/view.html"
     
 
-class AttachmentDeleteView(LoginRequiredMixin, MultiplePermissionsRequiredMixin, DeleteView):
+class AttachmentDeleteView(LoginRequiredMixin, MultiplePermissionsRequiredMixin, NextMixin, DeleteView):
     """
     Deletes an attachment from the storage backend.
     """
@@ -109,6 +124,8 @@ class AttachmentDeleteView(LoginRequiredMixin, MultiplePermissionsRequiredMixin,
     permissions = {
         "all": ("files.delete_attachment",)
     }
+    raise_exception = True  # If user is not allowed to edit the attachment,
+                            # return a HttpResponseForbidden response
     
     def dispatch(self, request, *args, **kwargs):
         self.kwargs = kwargs
@@ -134,18 +151,20 @@ class AttachmentDeleteView(LoginRequiredMixin, MultiplePermissionsRequiredMixin,
                 "attachments/%s/delete.html" % ctype.model,
                 "attachments/delete.html"
             ]
-            names = [p for p in template_search_list] + names
+            names = template_search_list + names
         return names
     
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
         self.object.delete()
 
-        # If success_url is provided, redirect.
-        # Else, render a default response using
-        # either a default or custom template.
-        if self.success_url:
-            return HttpResponseRedirect(self.success_url)
+        next_url = self.get_success_url()
+
+        # Redirect to "next" from POST data, or success_url.
+        # If neither is present, render a default response
+        # using a custom or default template.
+        if next_url:
+            return HttpResponseRedirect(next_url)
         else:
             ctype = self.object.content_type
             template_search_list = [

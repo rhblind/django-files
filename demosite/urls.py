22167a8ca3f0468235e651e9f93beea044083c03
Rolf Håvard Blindheim <rhblind@gmail.com>
2013-04-30 21:36:28 +0200

    creating new demosite

--- demosite/urls.py
+++ demosite/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from django.conf.urls.defaults import patterns, url
-from demosite.views import ShapeListView
+from django.conf.urls import patterns, url
+from demosite.views import ShapeDetailView
 
 admin.autodiscover()
 
-urlpatterns = patterns("demosite.views",
-    url(r"^$", view=ShapeListView.as_view(), name="home"),
-    url(r"^basic-usage/$", view=ShapeListView.as_view(template_name="basic-usage.html"), name="basic-usage"),
+urlpatterns = patterns(
+    "demosite.views",
+    url(r"^$", view=ShapeDetailView.as_view(), name="home"),
+    url(r"^form-example/$", view=ShapeDetailView.as_view(template_name="form_example.html"),
+        name="form_example"),
+    url(r"^edit-form-example/$", view=ShapeDetailView.as_view(template_name="edit_form_example.html"),
+        name="edit_form_example"),
+    url(r"^formset-example/$", view=ShapeDetailView.as_view(template_name="formset_example.html"),
+        name="formset_example"),
+    url(r"^listing-and-counting-example/$", view=ShapeDetailView.as_view(template_name="listing_example.html"),
+        name="listing_example"),
 )
--- demosite/views.py
+++ demosite/views.py
@@ -1,10 +1,26 @@
 # Create your views here.
 
-from django.views.generic.list import ListView
+from django.views.generic import DetailView
 from demosite.models import Shape
 
 
-class ShapeListView(ListView):
+class ShapeDetailView(DetailView):
+    """
+    The view
+    """
+
     model = Shape
-    context_object_name = "shapes"
     template_name = "home.html"
+
+    def get_object(self, queryset=None):
+
+        try:
+            obj = super(ShapeDetailView, self).get_object(queryset)
+        except AttributeError:
+            obj, created = Shape.objects.get_or_create(**{
+                "color": "green",
+                "shape": "square",
+                "descr": "A green square"
+            })
+        finally:
+            return obj
